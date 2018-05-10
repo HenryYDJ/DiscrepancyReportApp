ba661d3554391b1ea8fc31a5b03fbe7d3b16ab2a
Dingjun Yue <dyue@gatech.edu>
2018-05-10 15:49:09 -0500

    Modified create report page 1, added search for reports activity

---
+++ app/src/main/java/com/mooney/discrepancyreportapp/CreateNewReportActivity1.java
@@ -0,0 +1,78 @@
+package com.mooney.discrepancyreportapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class CreateNewReportActivity1 extends AppCompatActivity implements View.OnClickListener{
+    Button firstNextButton;
+    Button firstResetButton;
+
+    TextView partName;
+    TextView mooneyPartNumber;
+    TextView codeNumber;
+    TextView vendorNumber;
+    TextView vendorPartNumber;
+    TextView qty;
+    TextView wOrD;
+    TextView poOrSoNumber;
+    TextView vendor;
+    TextView model;
+    TextView unitSerialNumber;
+    TextView aircraftSerialNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_report1);
+
+        partName = (TextView)findViewById(R.id.partName);
+        mooneyPartNumber = (TextView)findViewById(R.id.mooneyPartNumber);
+        codeNumber = (TextView)findViewById(R.id.codeNumber);
+        vendorNumber = (TextView)findViewById(R.id.vendorNumber);
+        vendorPartNumber = (TextView)findViewById(R.id.vendorPartNumber);
+        qty = (TextView)findViewById(R.id.qty);
+        wOrD = (TextView)findViewById(R.id.wOrD);
+        poOrSoNumber = (TextView)findViewById(R.id.poOrSoNumber);
+        vendor = (TextView)findViewById(R.id.vendor);
+        model = (TextView)findViewById(R.id.model);
+        unitSerialNumber = (TextView)findViewById(R.id.unitSerialNumber);
+        aircraftSerialNumber = (TextView)findViewById(R.id.aircraftSerialNumber);
+
+        firstNextButton = (Button)findViewById(R.id.firstNextButton);
+        firstNextButton.setOnClickListener(this);
+
+        firstResetButton = (Button)findViewById(R.id.firstResetButton);
+        firstResetButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.firstNextButton:
+                Intent goToSecondPage = new Intent(this, CreateNewReportActivity2.class);
+                startActivity(goToSecondPage);
+                break;
+
+            case R.id.firstResetButton:
+                partName.setText(null);
+                mooneyPartNumber.setText(null);
+                codeNumber.setText(null);
+                vendorNumber.setText(null);
+                vendorPartNumber.setText(null);
+                qty.setText(null);
+                wOrD.setText(null);
+                poOrSoNumber.setText(null);
+                vendor.setText(null);
+                model.setText(null);
+                unitSerialNumber.setText(null);
+                aircraftSerialNumber.setText(null);
+                break;
+        }
+    }
+}
--- app/src/main/java/com/mooney/discrepancyreportapp/NavigationActivity.java
+++ app/src/main/java/com/mooney/discrepancyreportapp/NavigationActivity.java
@@ -1,13 +1,61 @@
 package com.mooney.discrepancyreportapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class NavigationActivity extends AppCompatActivity {
+public class NavigationActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener{
+    ImageButton createNewReportButton;
+    ImageButton myReportsButton;
+    ImageButton findAReportButton;
+    ImageButton myAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+        createNewReportButton = (ImageButton)findViewById(R.id.createNewReportButton);
+        createNewReportButton.setOnClickListener(this);
+        createNewReportButton.setOnLongClickListener(this);
+
+        myReportsButton = (ImageButton)findViewById(R.id.myReportsButton);
+        myReportsButton.setOnClickListener(this);
+
+        findAReportButton = (ImageButton)findViewById(R.id.searchForReportsButton);
+        findAReportButton.setOnClickListener(this);
+
+        myAccountButton = (ImageButton)findViewById(R.id.myAccountButton);
+        myAccountButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.createNewReportButton:
+                Intent createNewReportIntent = new Intent(this, CreateNewReportActivity1.class);
+                startActivity(createNewReportIntent);
+                break;
+
+            case R.id.myReportsButton:
+                break;
+
+            case R.id.searchForReportsButton:
+                Intent searchForReportIntent = new Intent(this, SearchForReportActivity.class);
+                startActivity(searchForReportIntent);
+                break;
+
+            case R.id.myAccountButton:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onLongClick(View v){
+        Toast toast = Toast.makeText(getApplicationContext(), "Stop pressing me NOW!", Toast.LENGTH_SHORT);
+        toast.show();
+        return true;
     }
 }
